6ced5fc81fa9c68c5460fa49f88d50566ccbc845
LilianaVasquezR <lilianavr1991@gmail.com>
2022-06-10 20:07:13 -0500

    Fin sección 8

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,20 +1,25 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const routes: Routes = [
     { 
         path:'dashboard',  // se queda como ruta por defecto 'dashboard' 
         component: PagesComponent,
         children: [
-            { path: '', component: DashboardComponent }, // se debe quedar vacio por el dashboard ya esta como ruta por defecto
-            { path: 'progress', component: ProgressComponent }, 
-            { path: 'grafica1', component: Grafica1Component },
-            { path: 'account-settings', component: AccountSettingsComponent },
+            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } }, // se debe quedar vacio por el dashboard ya esta como ruta por defecto
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' } },  // data: { titulo: 'Dashboard' } se utiliza para cambiar los titulos de cada pagina
+            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica 1' } },
+            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajuste de cuenta' } },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs' } },
         ]
     },
     
