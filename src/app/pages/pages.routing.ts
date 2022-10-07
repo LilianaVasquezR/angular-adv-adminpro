2864ea2e17675969330526c08c7f99b8555319f1
LilianaVasquezR <lilianavr1991@gmail.com>
2022-10-06 20:22:01 -0500

    Environment listo

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -4,21 +4,6 @@ import { NgModule } from '@angular/core';
 import { AuthGuard } from '../guards/auth.guard';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
-
-//Mantenimientos
-import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
-import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
-import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-import { MedicoComponent } from './mantenimientos/medicos/medico.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
-import { AdminGuard } from '../guards/admin.guard';
 
 
 
@@ -27,28 +12,10 @@ const routes: Routes = [
         path:'dashboard',  // se queda como ruta por defecto 'dashboard' 
         component: PagesComponent,
         canActivate: [ AuthGuard ],
-        children: [
-            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } }, // se debe quedar vacio por el dashboard ya esta como ruta por defecto
-            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajuste de cuenta' } },
-            { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'Busquedas' } },
-            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica 1' } },
-            { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
-            { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' } },  // data: { titulo: 'Dashboard' } se utiliza para cambiar los titulos de cada pagina
-            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
-            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs' } },
-
-            //  Mantenimientos
-            { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de Hospitales' } },
-            { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de Medicos' } },
-            { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Mantenimiento de Medicos' } },
-
-            // Rutas de admin
-            { path: 'usuarios', canActivate: [AdminGuard], component: UsuariosComponent, data: { titulo: 'Mantenimiento de Usuarios' } },
-           // { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Mantenimiento de Usuarios' } },
-        ]
-    },
-    
-   
+        canLoad:[AuthGuard],
+        //carga los hijos es una funcion de fecha para importar el modulo de manera perezosa
+       loadChildren: () => import('./child-routes.module').then( m => m.ChildRoutesModule )
+    },  
 ];
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  //base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminpro-backendd.herokuapp.com/api'  // esto este enviroment es para desplegar la aplicacion a produccion
 };
